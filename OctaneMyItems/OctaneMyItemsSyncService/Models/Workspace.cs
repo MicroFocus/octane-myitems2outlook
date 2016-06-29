e8530d05fde178f0b14f14d9d06c136d1c593eec
Wu, Binwen <binwen.wu@hpe.com>
2016-06-29 14:32:04 +0800

    Refractor Models

--- OctaneMyItems/OctaneMyItemsSyncService/Models/Workspace.cs
+++ OctaneMyItems/OctaneMyItemsSyncService/Models/Workspace.cs
@@ -2,22 +2,22 @@
 
 namespace OctaneMyItemsSyncService.Models
 {
-    public class Workspaces
-    {
-        public int total_count { get; set; }
-        public Workspace[] data { get; set; }
-        public bool exceeds_total_count { get; set; }
-    }
+  public class Workspaces
+  {
+    public int total_count { get; set; }
+    public Workspace[] data { get; set; }
+    public bool exceeds_total_count { get; set; }
+  }
 
-    public class Workspace
-    {
-        public string type { get; set; }
-        public DateTime creation_time { get; set; }
-        public string logical_name { get; set; }
-        public int version_stamp { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
-        public int id { get; set; }
-        public DateTime last_modified { get; set; }
-    }
+  public class Workspace
+  {
+    public string type { get; set; }
+    public DateTime? creation_time { get; set; }
+    public string logical_name { get; set; }
+    public int? version_stamp { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public int? id { get; set; }
+    public DateTime? last_modified { get; set; }
+  }
 }
