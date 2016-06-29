16a7bed70e2a56e8be09628402fa6ca06aedc613
xiajingc <jing-chun.xia@hpe.com>
2016-06-30 00:12:24 +0800

    add global category and define colors.

--- OctaneMyItems/OctaneMyItems/OctaneTask.cs
+++ OctaneMyItems/OctaneMyItems/OctaneTask.cs
@@ -4,79 +4,128 @@ using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace OctaneMyItems
 {
-  public class OctaneTask
-  {
-    public static Outlook.Application m_outlookApp;
-    public static void CreateTask(object octaneItem)
+    public class Constants
     {
-      m_outlookApp = Globals.ThisAddIn.Application.Application;
-      if (m_outlookApp == null)
-      {
-        m_outlookApp = new Outlook.Application();
-      }
+        //category constants
+        public const string CategoryOctaneBacklog = "[Octane]Backlog";
+        public const string CategoryOctaneTest = "[Octane]Test";
+        public const string CategoryOctaneRun = "[Octane]Run";
 
-      var taskList = m_outlookApp.Session.GetDefaultFolder(
-        Outlook.OlDefaultFolders.olFolderTasks) as Outlook.Folder;
-      var oTask = taskList.Items.Add("IPM.Task.Octane") as Outlook.TaskItem;
-
-
-      Outlook.UserProperty octane = oTask.UserProperties.Add("Octane", Outlook.OlUserPropertyType.olText);
-      octane.Value = JsonConvert.SerializeObject(octaneItem);
-
-      if (octaneItem is Backlog)
-      {
-        oTask.Categories = "[Octane]Backlog";
-        var backlog = octaneItem as Backlog;
-        oTask.Subject = "["+ backlog.subtype.ToUpper() + ":"+ backlog.id + "] " + backlog.name;
-        if ("Story".Equals(backlog.subtype, System.StringComparison.OrdinalIgnoreCase))
-          octane.ValidationText = "Story";
-        else
-          octane.ValidationText = "Defect";
-      }
-      else if (octaneItem is Run)
-      {
-        oTask.Categories = "[Octane]Run";
-        var run = octaneItem as Run;
-        oTask.Subject = "[" + run.id + "] " + run.name;
-        octane.ValidationText = "Run";
-      }
-      else if (octaneItem is Test)
-      {
-        oTask.Categories = "[Octane]Test";
-        var test = octaneItem as Test;
-        oTask.Subject = "[" + test.id + "] " + test.name;
-        octane.ValidationText = "Test";
-      }
-      oTask.Save();
-
-
-      //Outlook.Recipients oReceipients = oTask.Recipients;
-      //Outlook.Recipient oReceipient;
-      //oReceipient = oReceipients.Add("xintian@hotmail.com");
-      //oReceipient.Type = 1;
-      //oReceipients.ResolveAll();
-      //oTask.Assign();
-      //((Outlook._TaskItem)oTask).Send();
     }
-
-    public static void DeleteTask(string category)
+    public class OctaneTask
     {
-      m_outlookApp = Globals.ThisAddIn.Application.Application;
-      if (m_outlookApp == null)
-      {
-        m_outlookApp = new Outlook.Application();
-      }
 
-      var taskList = m_outlookApp.Session.GetDefaultFolder(
-        Outlook.OlDefaultFolders.olFolderTasks) as Outlook.Folder;
-      var oTasks = taskList.Items;
-      foreach (Outlook.TaskItem task in oTasks)
-      {
-        if (task.Categories == category)
+        public static Outlook.Application m_outlookApp;
+        public static void CreateTask(object octaneItem)
         {
-          task.Delete();
+            m_outlookApp = Globals.ThisAddIn.Application.Application;
+            if (m_outlookApp == null)
+            {
+                m_outlookApp = new Outlook.Application();
+            }
+
+            var taskList = m_outlookApp.Session.GetDefaultFolder(
+              Outlook.OlDefaultFolders.olFolderTasks) as Outlook.Folder;
+            var oTask = taskList.Items.Add("IPM.Task.Octane") as Outlook.TaskItem;
+
+
+            Outlook.UserProperty octane = oTask.UserProperties.Add("Octane", Outlook.OlUserPropertyType.olText);
+            octane.Value = JsonConvert.SerializeObject(octaneItem);
+
+            if (octaneItem is Backlog)
+            {
+                oTask.Categories = Constants.CategoryOctaneBacklog;
+                var backlog = octaneItem as Backlog;
+                oTask.Subject = "[" + backlog.subtype.ToUpper() + ":" + backlog.id + "] " + backlog.name;
+                if ("Story".Equals(backlog.subtype, System.StringComparison.OrdinalIgnoreCase))
+                    octane.ValidationText = "Story";
+                else
+                    octane.ValidationText = "Defect";
+            }
+            else if (octaneItem is Run)
+            {
+                oTask.Categories = Constants.CategoryOctaneRun;
+                var run = octaneItem as Run;
+                oTask.Subject = "[" + run.id + "] " + run.name;
+                octane.ValidationText = "Run";
+            }
+            else if (octaneItem is Test)
+            {
+                oTask.Categories = Constants.CategoryOctaneTest;
+                var test = octaneItem as Test;
+                oTask.Subject = "[" + test.id + "] " + test.name;
+                octane.ValidationText = "Test";
+            }
+            oTask.Save();
         }
-      }
+
+        public static void DeleteTask(string category)
+        {
+            m_outlookApp = Globals.ThisAddIn.Application.Application;
+            if (m_outlookApp == null)
+            {
+                m_outlookApp = new Outlook.Application();
+            }
+
+            var taskList = m_outlookApp.Session.GetDefaultFolder(
+              Outlook.OlDefaultFolders.olFolderTasks) as Outlook.Folder;
+            var oTasks = taskList.Items;
+            foreach (Outlook.TaskItem task in oTasks)
+            {
+                if (task.Categories == category)
+                {
+                    task.Delete();
+                }
+            }
+        }
+
+        public static void AddOctaneCategories()
+        {
+            m_outlookApp = Globals.ThisAddIn.Application.Application;
+            if (m_outlookApp == null)
+            {
+                m_outlookApp = new Outlook.Application();
+            }
+
+            Outlook.Categories categories =
+                m_outlookApp.Session.Categories;
+
+            string categoryName = Constants.CategoryOctaneBacklog;
+            if (!CategoryExists(categories, categoryName))
+            {
+                Outlook.Category category = categories.Add(categoryName,
+                    Outlook.OlCategoryColor.olCategoryColorDarkBlue);
+            }
+            categoryName = Constants.CategoryOctaneTest;
+            if (!CategoryExists(categories, categoryName))
+            {
+                Outlook.Category category = categories.Add(categoryName,
+                    Outlook.OlCategoryColor.olCategoryColorDarkGreen);
+            }
+            categoryName = Constants.CategoryOctaneRun;
+            if (!CategoryExists(categories, categoryName))
+            {
+                Outlook.Category category = categories.Add(categoryName,
+                    Outlook.OlCategoryColor.olCategoryColorYellow);
+            }
+        }
+
+        private static bool CategoryExists(Outlook.Categories categories, string categoryName)
+        {
+            try
+            {
+                Outlook.Category category =
+                    categories[categoryName];
+                if (category != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch { return false; }
+        }
     }
-  }
 }
--- OctaneMyItems/OctaneMyItems/ThisAddIn.cs
+++ OctaneMyItems/OctaneMyItems/ThisAddIn.cs
@@ -2,15 +2,15 @@
 
 namespace OctaneMyItems
 {
-  public partial class ThisAddIn
+    public partial class ThisAddIn
     {
         private static Configuration m_configuration;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
- 
-        m_configuration = new Configuration(this.Application);
- 
-    }
+
+            m_configuration = new Configuration(this.Application);
+
+        }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
@@ -19,131 +19,138 @@ namespace OctaneMyItems
         }
         public static Configuration Configuration
         {
-          get {                
-                return m_configuration; 
+            get
+            {
+                return m_configuration;
             }
         }
 
         public static void GetConfiguration()
         {
-     
-          m_configuration.GetConfiguration();
-       }
 
-    public static void ShowConfiguration()
-    {
-
-      m_configuration.ShowConfiguration();
-    }
-    public static async void SyncAll()
-    {
-      if (!m_configuration.IsInitialized)
-      {
-        m_configuration.GetConfiguration();
-      }
-      if (m_configuration.IsInitialized)
-      {
-        OctaneService octaneService = m_configuration.OctaneService;
-
-        // sync backlog item
-        OctaneTask.DeleteTask("[Octane]Backlog");
-        var myBacklogs = await octaneService.GetMyBacklogs();
-        foreach (OctaneMyItemsSyncService.Models.Backlog backlog in myBacklogs.data)
-        {
-          OctaneTask.CreateTask(backlog);
+            m_configuration.GetConfiguration();
         }
 
-        // sync run
-        OctaneTask.DeleteTask("[Octane]Run");
-        var runs = await octaneService.GetMyRuns();
-        foreach (OctaneMyItemsSyncService.Models.Run run in runs.data)
+        public static void ShowConfiguration()
         {
-          OctaneTask.CreateTask(run);
+
+            m_configuration.ShowConfiguration();
+        }
+        public static async void SyncAll()
+        {
+            if (!m_configuration.IsInitialized)
+            {
+                m_configuration.GetConfiguration();
+            }
+            if (m_configuration.IsInitialized)
+            {
+                OctaneService octaneService = m_configuration.OctaneService;
+                OctaneTask.AddOctaneCategories();
+
+                // sync backlog item
+                OctaneTask.DeleteTask(Constants.CategoryOctaneBacklog);
+                var myBacklogs = await octaneService.GetMyBacklogs();
+                foreach (OctaneMyItemsSyncService.Models.Backlog backlog in myBacklogs.data)
+                {
+                    OctaneTask.CreateTask(backlog);
+                }
+
+                // sync run
+                OctaneTask.DeleteTask(Constants.CategoryOctaneRun);
+                var runs = await octaneService.GetMyRuns();
+                foreach (OctaneMyItemsSyncService.Models.Run run in runs.data)
+                {
+                    OctaneTask.CreateTask(run);
+                }
+
+                // sync test
+                OctaneTask.DeleteTask(Constants.CategoryOctaneTest);
+                var tests = await octaneService.GetMyTests();
+                foreach (OctaneMyItemsSyncService.Models.Test test in tests.data)
+                {
+                    OctaneTask.CreateTask(test);
+                }
+
+            }
         }
 
-        // sync test
-        OctaneTask.DeleteTask("[Octane]Test");
-        var tests = await octaneService.GetMyTests();
-        foreach (OctaneMyItemsSyncService.Models.Test test in tests.data)
+        public static async void SyncBacklogItem()
         {
-          OctaneTask.CreateTask(test);
+            if (!m_configuration.IsInitialized)
+            {
+                m_configuration.GetConfiguration();
+            }
+            if (m_configuration.IsInitialized)
+            {
+                OctaneService octaneService = m_configuration.OctaneService;
+                OctaneTask.AddOctaneCategories();
+
+                OctaneTask.DeleteTask(Constants.CategoryOctaneBacklog);
+                var myBacklogs = await octaneService.GetMyBacklogs();
+                foreach (OctaneMyItemsSyncService.Models.Backlog backlog in myBacklogs.data)
+                {
+                    OctaneTask.CreateTask(backlog);
+                }
+            }
+
+        }
+        public static async void SyncTest()
+        {
+            if (!m_configuration.IsInitialized)
+            {
+                m_configuration.GetConfiguration();
+            }
+            if (m_configuration.IsInitialized)
+            {
+                OctaneService octaneService = m_configuration.OctaneService;
+                OctaneTask.AddOctaneCategories();
+
+                OctaneTask.DeleteTask(Constants.CategoryOctaneTest);
+                var tests = await octaneService.GetMyTests();
+                foreach (OctaneMyItemsSyncService.Models.Test test in tests.data)
+                {
+                    OctaneTask.CreateTask(test);
+                }
+            }
         }
 
-      }
-    }
-
-    public static async void SyncBacklogItem()
-    {
-      if (!m_configuration.IsInitialized)
-      {
-        m_configuration.GetConfiguration();
-      }
-      if (m_configuration.IsInitialized)
-      {
-        OctaneService octaneService = m_configuration.OctaneService;
-        OctaneTask.DeleteTask("[Octane]Backlog");
-        var myBacklogs = await octaneService.GetMyBacklogs();
-        foreach(OctaneMyItemsSyncService.Models.Backlog backlog in myBacklogs.data)
+        public static async void SyncRun()
         {
-          OctaneTask.CreateTask(backlog);
+            if (!m_configuration.IsInitialized)
+            {
+                m_configuration.GetConfiguration();
+            }
+            if (m_configuration.IsInitialized)
+            {
+                OctaneService octaneService = m_configuration.OctaneService;
+                OctaneTask.AddOctaneCategories();
+
+                OctaneTask.DeleteTask(Constants.CategoryOctaneRun);
+                var runs = await octaneService.GetMyRuns();
+                foreach (OctaneMyItemsSyncService.Models.Run run in runs.data)
+                {
+                    OctaneTask.CreateTask(run);
+                }
+            }
         }
-      }
-    
-    }
-    public static async void SyncTest()
-    {
-      if (!m_configuration.IsInitialized)
-      {
-        m_configuration.GetConfiguration();
-      }
-      if (m_configuration.IsInitialized)
-      {
-        OctaneService octaneService = m_configuration.OctaneService;
-        OctaneTask.DeleteTask("[Octane]Test");
-        var tests = await octaneService.GetMyTests();
-        foreach (OctaneMyItemsSyncService.Models.Test test in tests.data)
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
-          OctaneTask.CreateTask(test);
+            return new Ribbon2();
         }
-      }
-    }
+        #region VSTO generated code
 
-    public static async void SyncRun()
-    {
-      if (!m_configuration.IsInitialized)
-      {
-        m_configuration.GetConfiguration();
-      }
-      if (m_configuration.IsInitialized)
-      {
-        OctaneService octaneService = m_configuration.OctaneService;
-        OctaneTask.DeleteTask("[Octane]Run");
-        var runs = await octaneService.GetMyRuns();
-        foreach (OctaneMyItemsSyncService.Models.Run run in runs.data)
-        {
-          OctaneTask.CreateTask(run);
-        }
-      }
-    }
-
-
-    protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
-    {
-      return new Ribbon2();
-    }
-    #region VSTO generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InternalStartup()
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
         {
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
         }
-        
+
         #endregion
     }
-  
+
 }
