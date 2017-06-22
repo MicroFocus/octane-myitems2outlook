960e3208afebabb5fbd4415d77901a9a1c5741a6
Spencer Bruce <spencer.bruce@hpe.com>
2017-06-22 10:06:34 +0300

    Updated version to 12.55.3

--- OctaneMyItems/OctaneMyItems/Properties/AssemblyInfo.cs
+++ OctaneMyItems/OctaneMyItems/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.53.21.0")]
-[assembly: AssemblyFileVersion("12.53.21.0")]
+[assembly: AssemblyVersion("12.55.3.0")]
+[assembly: AssemblyFileVersion("12.55.3.0")]
 
